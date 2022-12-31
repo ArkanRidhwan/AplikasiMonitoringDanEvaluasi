a5380396a2374758f6d53cca41feab2f34322a95
Irwan Hermawan <rwnhermawan@gmail.com>
2022-12-31 09:01:15 +0700

    upload document, video, image

--- app/src/main/java/com/example/aplikasimonitoringdanevaluasi/utils/Support.kt
+++ app/src/main/java/com/example/aplikasimonitoringdanevaluasi/utils/Support.kt
@@ -2,11 +2,19 @@ package com.example.aplikasimonitoringdanevaluasi.utils
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.util.Base64
 import android.view.View
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.UploadTask
+import com.google.firebase.storage.ktx.storage
 import java.io.ByteArrayOutputStream
+import java.io.File
 
 private lateinit var preferenceManager: PreferenceManager
 
@@ -33,10 +41,26 @@ fun EditText.error(text: String) {
     requestFocus()
 }
 
-// Compress Size
-fun encodeImage(bm: Bitmap): String {
+fun ImageView.loadCircleImageFromUri(uri: Uri?) {
+    Glide.with(context)
+        .load(uri)
+        .into(this)
+}
+
+fun Bitmap.encodeImage(): String {
     val byteArrayOutputStream = ByteArrayOutputStream()
-    bm.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+    this.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
     val b = byteArrayOutputStream.toByteArray()
     return Base64.encodeToString(b, Base64.DEFAULT)
+}
+
+fun ImageView.uploadImage(file: File): UploadTask {
+    val storageRef = Firebase.storage.reference
+    val imagesRef = storageRef.child("images/${file.name}")
+
+    val bitmapImage = (drawable as BitmapDrawable).bitmap
+    val baos = ByteArrayOutputStream()
+    bitmapImage.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+    val dataBaos = baos.toByteArray()
+    return imagesRef.putBytes(dataBaos)
 }
